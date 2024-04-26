da9c33cf4b45e15a2c9af44dbb3ae4861e65923b
Sascha Schuenemann <sascha.schuenemann.extern@pflege.de>
2024-04-26 15:34:19 +0200

    initial version of simple laravel service

---
+++ hello-world-api/app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    public function helloWorld()
+    {
+        return response()->json(['message' => 'hello world'], 200);
+    }
+}
